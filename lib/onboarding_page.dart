8847f7a8c3b2b6fd23d0bba6b36edd607933220d
hollali <dheztinykartel@gmail.com>
2025-07-31 09:30:24 +0000

    `Refactored OnboardingPage widget: updated image asset, added text and button, and modified styles.`

--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -13,27 +13,54 @@ class OnboardingPage extends StatelessWidget {
             child: Container(
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage('assets'),
+                  image: AssetImage('assets/onboarding.png'),
                   fit: BoxFit.cover,
                 ),
               ),
             ),
           ),
           Expanded(
-          child:Container(
-            child: Column(
-              children: [
-                Text(
-                  'Premium cars at affordable prices. \nEnjoy the luxury of driving without the hassle of ownership.',
-                  style: TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
+            child: Container(
+              child: Column(
+                children: [
+                  Text(
+                    'Premium cars at affordable prices. \nEnjoy the luxury of driving without the hassle of ownership.',
+                    style: TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
                   ),
-                ),
-              ],
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Premium and prestige car rentals in Ghana. \nExperience the thrill of driving luxury vehicles without the commitment of ownership.',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 320,
+                    height: 54,
+                    child: ElevatedButton(onPressed: () {
+                      Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(builder: (context) => OnboardingPage()),
+                        (Route<dynamic> route) => false,
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      backgroundColor: Colors.white
+                    ),
+                    child: Text(
+                      'Get Started',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                  ))
+                ],
+              ),
             ),
-          ),
           ),
         ],
       ),
